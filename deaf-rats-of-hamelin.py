98c250800512a2c60a2253a34a8b004ecc14d8a0
Robert Joseph <91274865+roberttomjoseph@users.noreply.github.com>
2021-10-31 01:26:11 +0530

    started deaf-rats kata

---
+++ deaf-rats-of-hamelin.py
@@ -0,0 +1,14 @@
+def count_deaf_rats(town):
+    num = len(town)
+    rat_array_before = []
+    rat_array_after = []
+    piper_pos = town.index('P')/2 + 1
+    for character in town:
+        if character != 'P':
+            rat_array_before.append(character)
+        elif character == 'P':
+            breakxw
+    print(rat_array_after)
+
+
+count_deaf_rats("~O~O~O~OP~O~OO~")
