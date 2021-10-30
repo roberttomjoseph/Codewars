4821b905e88b3aa61298e32097fb414709cb7557
Robert Joseph <91274865+roberttomjoseph@users.noreply.github.com>
2021-10-31 01:59:17 +0530

    Update deaf-rats-of-hamelin.py

--- deaf-rats-of-hamelin.py
+++ deaf-rats-of-hamelin.py
@@ -1,14 +1,35 @@
 def count_deaf_rats(town):
-    num = len(town)
-    rat_array_before = []
-    rat_array_after = []
-    piper_pos = town.index('P')/2 + 1
-    for character in town:
-        if character != 'P':
-            rat_array_before.append(character)
-        elif character == 'P':
-            breakxw
-    print(rat_array_after)
+    rat_list = []
 
+    for character in town:
+        if character != ' ':
+            if character != 'P':
+                if len(rat_list) == 0:
+                    rat_list.append(character)
+                elif len(rat_list[-1]) == 1:
+                    rat_list[-1] = rat_list[-1] + character
+                elif len(rat_list[-1]) == 2:
+                    rat_list.append(character)
+            elif character == 'P':
+                    rat_list.append('TP')
+
+    if rat_list.index('TP') == 0:
+        before_list = []
+    else:
+        before_list = rat_list[:rat_list.index('TP')]
+
+    after_list = rat_list[rat_list.index('TP') + 1:]
+
+    count = 0
+
+    for rat in before_list:
+        if rat == 'O~':
+            count += 1
+    for rat in after_list:
+        print(rat)
+        if rat == '~O':
+            count += 1
+    
+    return count
 
 count_deaf_rats("~O~O~O~OP~O~OO~")
